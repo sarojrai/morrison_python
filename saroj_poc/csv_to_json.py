c3b45837fc49fe19d3351ae3601e8bf06cdba4f9
sarojrai <rai.saroj20@gmail.com>
2020-06-22 13:33:30 +0530

    log and  error handling

--- saroj_poc/csv_to_json.py
+++ saroj_poc/csv_to_json.py
@@ -4,23 +4,21 @@ import os
 import csv
 import json
 import logging
-import collections.abc
+
 
 logging.basicConfig(filename='csv_to_json.log',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-CSV_FILE = "data.csv"
-INDEX_MAP, JSON_DATA = {}, []
-filed_list = ["name","id","link"]
-field_list_count = len(filed_list)
+CSV_FILE = "data1.csv"
+PARENT_MAP, JSON_DATA = {}, []
+
+FIELDS = ["name", "id", "link"]
+FIELD_LEN = len(FIELDS)
 
 
 file_exist = True
 try:
     fp = open(CSV_FILE, 'r')
-    
-    
 except FileNotFoundError:
-    
     file_exist = False
     logging.error(' File not found. Check the name of file')
 
@@ -28,39 +26,33 @@ if file_exist == True:
 
     for each_row in csv.reader(fp):
         column_length = len(each_row)
-        no_of_branches = int(column_length / field_list_count)
-        start_range = 0
-        end_range = field_list_count
-        index_list = []
+        if column_length < FIELD_LEN:
+            continue
+        i = FIELD_LEN
+        parent_name, parent_id, parent_link = each_row[ : i]
+        if not PARENT_MAP.__contains__(parent_id):
+            children = []
+            JSON_DATA.append({"id": parent_id, "name": parent_name, "link": parent_link, "children": children})
+            PARENT_MAP[parent_id] = children
         
-        for i in range(0,no_of_branches):
+        
+        while i < column_length:
             
-            current_pos = i + int(1)
-            if i == 0:
-                name1, id1, url1 = each_row[start_range: end_range]
-                index1 = INDEX_MAP.get(id1, -1)
+            child_name, child_id, child_link = each_row[i : (i + FIELD_LEN)]
+            if  all(each_row[i : (i + FIELD_LEN)]):
+                children = []
+                if not PARENT_MAP.__contains__(child_id):
+                    PARENT_MAP[parent_id].append({"id": child_id, "name": child_name, "link": child_link, "children": children})
+                    PARENT_MAP[child_id] = children
                 
-                if index1 == -1:
-                    JSON_DATA.append({"id": id1, "link": url1, "name": name1, "children": []})
-                    index1 = INDEX_MAP[id1] = len(JSON_DATA) - 1
-                    index_list.append([["index" + str(current_pos)],["children"]])
-            elif i == no_of_branches - int(1):
-                
-                name3, id3, url3 = each_row[start_range : end_range]
-                JSON_DATA[index1]["children"][index2]["children"].append({"id": id3, "link": url3, "name": name3, "children": []})
-            else:
-                
-                name1, id1, url1 = each_row[start_range : end_range]
-                index2 = INDEX_MAP.get(id1, -1)
-                if index2 == -1:
-                    JSON_DATA[index1]["children"].append({"id": id1, "link": url1, "name": name1, "children": []})
-                    index2 = INDEX_MAP[id1] = len(JSON_DATA[index1]["children"]) - 1
 
-            start_range += field_list_count
-            end_range += field_list_count
+            parent_id, i = child_id, (i + FIELD_LEN)
+        
 
         
-       
+
+
+
     fp.close()
     try: 
         json_data=json.dumps(JSON_DATA[1 : ], indent=4)
